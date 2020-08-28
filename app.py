0a5cbdb44b59690e002bf05fa19dbc2bb67ef999
wtopor <sherlock@holmes.com>
2020-08-28 21:49:05 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import models.py
+
+if __name__ == '__main__':
+    recruiter_one = models.Recruiter('mary', 'james', 'maryjames@mail.com', '932569357', '500', '5')
+    programmer_one = models.Programmer('mick', 'colins', 'nickcolins@mail.com', '823659256', '600',
+                                       ['Java', 'C#', 'C++'], '5')
+    programmer_two = models.Programmer('leo', 'luck', 'leoluck@mail.com', '283569234', '450',
+                                       ['Python', 'Java', 'PHP', 'Swift'], '10')
+    candidate_one = models.Candidate('Edward Grey', 'edwardgrey@mail.com', ['Python', 'HTML', 'C#'], 'Python',
+                                     'Upper Intermediate')
+    candidate_two = models.Candidate('Simon Teon', 'simonteon@mail.com', ['Python', 'Java', 'SQL'], 'Java',
+                                     'Upper Intermediate')
+    candidate_three = models.Candidate('Christin May', 'christinmay@mail.com', ['JavaScript', 'PHP'], 'JavaScript',
+                                       'Upper Intermediate')
---
+++ models.py
@@ -0,0 +1,89 @@
+from datetime import datetime, date
+
+
+class Employee:
+    def __init__(self, name, surname, email, phone, salary):
+        self.name = name
+        self.surname = surname
+        self.email = email
+        self.phone = phone
+        self.salary = salary
+
+    def work(self):
+        return 'I came to the office'
+
+    def salary_check(self, salary, days):
+        return salary * days
+
+    def __lt__(self, other):
+        return self.salary < other.salary
+
+    def __gt__(self, other):
+        return self.salary > other.salary
+
+    def __eq__(self, other):
+        return self.salary == other.salary
+
+    def check_salary(self, salary):
+        quantity_of_days = date.today() - date(year=date.today().year, month=date.today().month, day=1)
+        quantity_of_weeks = (quantity_of_days.days + 1) // 7
+        quantity_of_days_offs = quantity_of_weeks * 2
+        quantity_of_business_days = quantity_of_days.days - quantity_of_days_offs
+        return quantity_of_business_days * salary
+
+
+class Recruiter(Employee):
+
+    def __init__(self, name, surname, email, phone, salary, hired_this_month):
+        Employee.__init__(self, name, surname, email, phone, salary)
+        self.hired_this_month = hired_this_month
+
+    def work(self):
+        return super().work() + 'and start hiring'
+
+    def __str__(self):
+        return 'Recruiter : ' + self.name.capitalize() + ' ' + self.surname.capitalize()
+
+
+class Programmer(Employee):
+
+    def __init__(self, name, surname, email, phone, salary, tech_stack, closed_this_month):
+        Employee.__init__(self, name, surname, email, phone, salary)
+        self.tech_stack = tech_stack
+        self.closed_this_month = int(closed_this_month)
+
+    def work(self):
+        return super().work() + 'and start programing'
+
+    def __str__(self):
+        return 'Programmer : ' + self.name.capitalize() + ' ' + self.surname.capitalize()
+
+    def __lt__(self, other):
+        return self.tech_stack < other.tech_stack
+
+    def __gt__(self, other):
+        return self.tech_stack > other.tech_stack
+
+    def __eq__(self, other):
+        return self.tech_stack == other.self_stack
+
+    def alfa_programmer(self, other):
+        return set(self.tech_stack + other.tech_stack), self.closed_this_month + other.closed_this_month
+
+
+class Candidate:
+
+    def __init__(self, full_name, email, technologies, main_skill, main_skill_grade):
+        self.full_name = full_name
+        self.email = email
+        self.technologies = technologies
+        self.main_skill = main_skill
+        self.main_skill_grade = main_skill_grade
+
+
+class Vacancy:
+
+    def __init__(self, title, main_skill, main_skill_level):
+        self.title = title
+        self.main_skill = main_skill
+        self.main_skill_level = main_skill_level
